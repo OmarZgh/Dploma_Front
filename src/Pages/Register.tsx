6774bccc754009f988019fa7c41c6ac7787c681d
Mickaelfbr <82059684+mickaelfbr@users.noreply.github.com>
2023-03-06 07:38:07 +0100

    Ajout page modify and delete

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -5,19 +5,33 @@ import {useState} from "react";
 const Register = () => {
 
     const [open, setOpen] = useState(false);
+
+    function openTemplateForm() {
+        setOpen(true)
+    }
+
+    function openDiplomeForm() {
+        setOpen(true)
+    }
+
     return (<Container  style={{display:"flex"}}>
 
             <Grid container spacing={2}>
                 <Grid item xs={1} lg={4}>
-                    <ButtonModal title={"Template"} description={"Test de descreuotio kljhbkhsbkd kbkjhsbfkhbcskjhc slijbfclksjhbdl"} image={"https://picsum.photos/200"}>
-                        <Button variant={"contained"}>Helo</Button>
+                    <ButtonModal title={"Template"} description={"Créer une template de diplôme"} image={"https://picsum.photos/200"}>
+                        <Button onClick={openTemplateForm} variant={"contained"}>Créer</Button>
                     </ButtonModal>
                 </Grid>
                 <Grid item xs={1} lg={4}>
-                    <ButtonModal title={"Template"} description={""}/>
+                    <ButtonModal title={"Certification"} description={"Nouvelle certification"}>
+                        <Button onClick={openDiplomeForm} variant={"contained"}>Créer</Button>
+                    </ButtonModal>
                 </Grid>
                 <Grid item xs={1} lg={4}>
-                    <ButtonModal title={"Template"} description={""}/>
+                    <ButtonModal title={"Template & certification"} description={"Vous avez la possibilité de créer une template et ajouter une certification"}>
+                        <Button onClick={openTemplateForm} variant={"contained"}>Créer</Button>
+                    </ButtonModal>
+
                 </Grid>
 
             </Grid>
