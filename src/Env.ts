b9d3910e5ae1bdd277ad2dba854411379138ab12
Archyb <121549285+Archyb@users.noreply.github.com>
2023-03-30 08:48:05 +0200

    solve routing issue with component find and explore2

--- src/Env.ts
+++ src/Env.ts
@@ -3,6 +3,6 @@ export namespace Env {
     const API_KEY = "";
     const PRIVATE_KEY = "";
     const CONTRACT_ADDRESS = "";
-    export const DOMAIN="https://react.nogadev,ch";
+    export const DOMAIN="https://react.nogadev.ch";
 
 }
