ea6a1f62a449bad56a866e918e21fa49b86b4ce5
Archyb <121549285+Archyb@users.noreply.github.com>
2023-04-24 19:12:01 +0200

    add first wirte method

--- src/Services/Web3APi.ts
+++ src/Services/Web3APi.ts
@@ -1,14 +1,24 @@
 import Web3 from "web3";
 import { AbiItem } from 'web3-utils'
 import abi from "../ABI/ABI.json";
-import {IDploma} from "../type";
+import {IDploma, ITemplate} from "../type";
+import {useSmc} from "../hooks/useSmc";
 
 const web3 = new Web3("https://eth-goerli.g.alchemy.com/v2/GoPk5qRGiHAo9EUXOvUquXJFW-MsRg5i");
 const contractAddress="0xf87E2fD2214F291eE6098A091BA42DC01c002d7A"
 const dploma = new web3.eth.Contract(abi as unknown as AbiItem[], contractAddress);
-
+const sender=(window as any).ethereum.request({method: "eth_requestAccounts"}).then((res:any)=>{console.log(res)}).catch((err:any)=>{console.log(err)})
 
 export const getDploma = async (id: string) => {
     const diploma:IDploma = await dploma.methods.getCertification(id).call();
+    return diploma;
+}
+const addTemplate = async ( title: string, name: string, date: string, spec: string[]) => {
+    const diploma:ITemplate = await dploma.methods.connect(sender).addTemplate(title, name, date, spec).call();
+    return diploma;
+}
+
+const insertWithTemplate = async (id: string, templateId: string, name: string, date: string, spec: string[]) => {
+    const diploma:IDploma = await dploma.methods.connect(sender).insertWithTemplate(id, templateId, name, date, spec).call();
     return diploma;
 }
