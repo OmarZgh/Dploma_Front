1b0eb813d37a2b22fbc71285a35fd9e116606d7b
Archyb <121549285+Archyb@users.noreply.github.com>
2023-05-19 09:20:58 +0200

    add alchemy to env

--- src/Services/Web3APi.ts
+++ src/Services/Web3APi.ts
@@ -3,18 +3,19 @@ import {AbiItem} from 'web3-utils'
 import abi from "../ABI/ABI.json";
 import {IDploma} from "../Type/type";
 
-const alchemyKey = new Web3("https://eth-sepolia.g.alchemy.com/v2/P6zmb6lMhjRLlQKIgIFcSZ8zuG-Q2Sq0")
-const web3 = new Web3((window as any).ethereum)
+const alchemyUrl = process.env.REACT_APP_ALCHEMY_URL;
+const getterProvider = new Web3(alchemyUrl as string)
+const provider = new Web3((window as any).ethereum)
 const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
-const dploma = new web3.eth.Contract(abi as AbiItem[], contractAddress);
-const test=  new alchemyKey.eth.Contract(abi as AbiItem[], contractAddress);
+const dploma = new provider.eth.Contract(abi as AbiItem[], contractAddress);
+const dplomaGetter=  new getterProvider.eth.Contract(abi as AbiItem[], contractAddress);
 
 export const getDploma = async (id: string) => {
-    const diploma: IDploma = await test.methods.getCertification(id).call();
+    const diploma: IDploma = await dplomaGetter.methods.getCertification(id).call();
     return diploma;
 }
 export const addTemplate = async (title: string, name: string, date: number, spec: string[]) => {
-    const sender = await web3.eth.getAccounts()
+    const sender = await provider.eth.getAccounts()
     return await dploma.methods.createTemplate(title, name, date, spec).send({from: sender[0]});
 }
 
@@ -26,7 +27,7 @@ export const insertWithTemplate = async (cfiedfirstname: string,
                                          hashTemplate: string,
                                          certifiedPubAdress: string,
 ) => {
-    const sender = await web3.eth.getAccounts()
+    const sender = await provider.eth.getAccounts()
     return await dploma.methods.insertWithTemplate(cfiedfirstname,
         cfiedLastname,
         cfiedBirthdate,
@@ -46,7 +47,7 @@ export const insertWithoutTemplate = async (cfiedfirstname: string,
                                             tempName: string,
                                             tempDate: number,
                                             tempSpecs: string[]) => {
-    const sender = await web3.eth.getAccounts()
+    const sender = await provider.eth.getAccounts()
     return await dploma.methods.insertWithoutTemplate(
         cfiedfirstname,
         cfiedLastname,
@@ -65,7 +66,7 @@ export const updateTemplate = async (hashTemplate: string,
                                      name: string,
                                      date: number,
                                      spec: string[]) => {
-    const sender = await web3.eth.getAccounts()
+    const sender = await provider.eth.getAccounts()
     console.log(hashTemplate, title, name, date, spec)
     const diploma = await dploma.methods.ModifyTemplate(
         hashTemplate,
@@ -77,11 +78,11 @@ export const updateTemplate = async (hashTemplate: string,
 }
 
 export const deleteCertif = async (id: string) => {
-    const sender = await web3.eth.getAccounts()
+    const sender = await provider.eth.getAccounts()
     return await dploma.methods.DeleteCertif(id).send({from: sender[0]});
 }
 
 export const changeVisibility = async (id: string) => {
-    const sender = await web3.eth.getAccounts()
+    const sender = await provider.eth.getAccounts()
     return await dploma.methods.toggleStudentVisibility(id).send({from: sender[0]});
 }
